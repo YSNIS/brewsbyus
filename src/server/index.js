2bf96ea23c6e136d8a9967846b6451ff03233c11
Jacob Fleisher <jacobfleisher@Jacobs-MacBook-Pro.local>
2018-09-08 21:34:34 -0400

    still workin on heroku deploy"

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,6 @@ const os = require('os');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    console.log('Express: production');
-} else {
-    app.use(express.static('dist'));
-    console.log('Express: dist');    
-}
+app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}`));
