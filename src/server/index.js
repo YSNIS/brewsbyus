b67a6df022e6b26a2b9516771437c92a45ad83c4
Jacob Fleisher <jfleish1@gmail.com>
2018-09-12 21:23:42 -0400

    Adding postgres, todo db, table, and routing for testing on heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,30 @@
+require("@babel/register");
+
 const express = require('express');
 const os = require('os');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const env = require('dotenv').config().parsed;
 
 const app = express();
 
+// Log requests to the console.
+app.use(logger('dev'));
+
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static('dist'));
+
+// Grab routes from ./server/routes
+require('./routes')(app);
+
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/test', (req, res) => res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.',
+}));
+
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}`));
