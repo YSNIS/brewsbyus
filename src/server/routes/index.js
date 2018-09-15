80a6bcc31cf4137d1335b466d138a0436ced36ad
Jacob Fleisher <jfleish1@gmail.com>
2018-09-15 13:56:50 -0400

    server routing reload

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,8 @@
-const todosController = require('../controllers').todos;
+import todosController from '../controllers/todos';
+import path from 'path';
 
 module.exports = (app) => {
-  
+
   // API ROUTES
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
@@ -9,4 +10,14 @@ module.exports = (app) => {
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
+
+  // Default CATCH-ALL
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../../dist/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 };
