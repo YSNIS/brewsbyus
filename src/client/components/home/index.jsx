14b84abcfcc45313a30261ead7e74fbfd822ba28
Jacob Fleisher <jfleish1@gmail.com>
2018-09-15 15:29:16 -0400

    local api call test

--- src/client/components/home/index.jsx
+++ src/client/components/home/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class Home extends React.Component {
+
+    componentDidMount() {
+        const url = process.env.API_URL;
+
+        fetch(`${url}/api/todos`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ data })
+                console.log(this.state);
+            });
+    }
+
     render() {
         return (
             <h1>Homepage</h1>
