1176c93e27ddd3408cb601de5c1cdcf5783de06c
Jacob Fleisher <jfleish1@gmail.com>
2018-09-15 16:33:04 -0400

    Updating local environment: added cors for localhost:3000 to express.

--- src/server/index.js
+++ src/server/index.js
@@ -17,6 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('dist'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // Grab routes from ./server/routes
 require('./routes')(app);
 
