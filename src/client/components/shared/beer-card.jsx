8ca08126e0b9630375eaed0c539a29fa9ac6650d
Jacob Fleisher <jfleish1@gmail.com>
2018-09-29 17:13:18 -0400

    adding modal to beer card

--- src/client/components/shared/beer-card.jsx
+++ src/client/components/shared/beer-card.jsx
@@ -8,6 +8,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteBorder';
 import classNames from 'classnames';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import Modal from '@material-ui/core/Modal';
 
 
 const styles = theme => ({
@@ -59,6 +60,13 @@ const styles = theme => ({
         alignItems: 'center',
         justifyContent: 'center',
         height: 54
+    },
+    modal: {
+        background: 'white',
+        width: '50%',
+        height: '50%',
+        margin: 'auto',
+        marginTop: 100
     }
 });
 
@@ -67,25 +75,31 @@ class BeerCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            open: false,
             beer: props.beer
         }
     }
 
     render() {
 
+        function getModalStyle() {
+            const top = 50;
+            const left = 50;
+
+            return {
+                top: `${top}%`,
+                left: `${left}%`,
+                transform: `translate(-${top}%, -${left}%)`,
+            };
+        }
+
         let self = this;
 
-        const { classes, styles, beer } = this.props;        
+        const { classes, styles, beer } = this.props;
 
-        function open(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            console.log('Open')
-        }
-    
         function saveToFavorites(e) {
             e.stopPropagation();
-            console.log('Save');       
+            console.log('Save');
             self.setState({
                 beer: {
                     ...self.beer,
@@ -94,48 +108,71 @@ class BeerCard extends React.Component {
             });
         }
 
+        function handleOpen(e) {
+            e.stopPropagation();
+            console.log('hi');
+            self.setState({ open: true });
+        };
+
+        function handleClose() {
+            self.setState({ open: false });
+        };
+
         return (
-            <Paper style={styles} className={classes.root} onClick={open}>
-                <Typography variant="body2" className={classNames(classes['card-font'], classes.title)}>{beer.name}</Typography>
-                { self.state.beer.favorite && 
-                    <IconButton aria-label="FavoriteIcon" onClick={saveToFavorites} className={classes.favorite}> 
-                        <FavoriteIcon/>
-                    </IconButton>
-                }
-                { !self.state.beer.favorite &&
-                    <IconButton aria-label="FavoriteOutlinedIcon" onClick={saveToFavorites} className={classes.favorite}> 
-                        <FavoriteOutlinedIcon/>
-                    </IconButton>
-                }
-                <Typography 
-                    variant="body2" 
-                    className={classNames(classes['card-font'], classes.price)}>
-                    ${(beer.price/100).toFixed(2)}
-                </Typography>    
-                <div style={{'display': 'flex'}}>
-                    <img src="https://media.gettyimages.com/photos/beer-glass-with-overflowing-foam-picture-id183341783" />
-                    <div>
-                        <Typography variant="body2" style={{'textAlign': 'center'}} className={classes['card-font']}>Type: {beer.type}</Typography>
-                        <ul className={classes['attr-list']}>
-                            <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[0]}</Typography></li>
-                            <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[1]}</Typography></li>
-                            <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[2]}</Typography></li>
-                        </ul>
+            <React.Fragment>
+                <Paper style={styles} className={classes.root} onClick={handleOpen}>
+                    <Typography variant="body2" className={classNames(classes['card-font'], classes.title)}>{beer.name}</Typography>
+                    {self.state.beer.favorite &&
+                        <IconButton aria-label="FavoriteIcon" onClick={saveToFavorites} className={classes.favorite}>
+                            <FavoriteIcon />
+                        </IconButton>
+                    }
+                    {!self.state.beer.favorite &&
+                        <IconButton aria-label="FavoriteOutlinedIcon" onClick={saveToFavorites} className={classes.favorite}>
+                            <FavoriteOutlinedIcon />
+                        </IconButton>
+                    }
+                    <Typography
+                        variant="body2"
+                        className={classNames(classes['card-font'], classes.price)}>
+                        ${(beer.price / 100).toFixed(2)}
+                    </Typography>
+                    <div style={{ 'display': 'flex' }}>
+                        <img src="https://media.gettyimages.com/photos/beer-glass-with-overflowing-foam-picture-id183341783" />
+                        <div>
+                            <Typography variant="body2" style={{ 'textAlign': 'center' }} className={classes['card-font']}>Type: {beer.type}</Typography>
+                            <ul className={classes['attr-list']}>
+                                <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[0]}</Typography></li>
+                                <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[1]}</Typography></li>
+                                <li><Typography variant="body2" className={classes['card-font']}>{beer.attributes[2]}</Typography></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className={classes.statusGroup}>
-                    <Typography variant="body2" className={classes['card-font']}>{beer.status}</Typography>            
-                    { beer.status === 'Brewing' &&                     
-                        <React.Fragment>
-                            <Typography variant="body2" className={classes['card-font']}>Brewed By: <span style={{color: '#3570ce'}}>{beer.brewer}</span></Typography> 
-                            <Typography variant="body2" className={classes['card-font']}>{beer.totalToBeMade - beer.committed} Left!</Typography>
-                        </React.Fragment>
-                    }
-                    { beer.status !== 'Brewing' &&
-                        <Typography variant="body2" className={classes['card-font']}>{beer.committed} Committed!</Typography>                        
-                    }
-                </div>
-            </Paper>
+                    <div className={classes.statusGroup}>
+                        <Typography variant="body2" className={classes['card-font']}>{beer.status}</Typography>
+                        {beer.status === 'Brewing' &&
+                            <React.Fragment>
+                                <Typography variant="body2" className={classes['card-font']}>Brewed By: <span style={{ color: '#3570ce' }}>{beer.brewer}</span></Typography>
+                                <Typography variant="body2" className={classes['card-font']}>{beer.totalToBeMade - beer.committed} Left!</Typography>
+                            </React.Fragment>
+                        }
+                        {beer.status !== 'Brewing' &&
+                            <Typography variant="body2" className={classes['card-font']}>{beer.committed} Committed!</Typography>
+                        }
+                    </div>
+                </Paper>
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={self.state.open}
+                    onClose={handleClose}
+                >
+                    <div className={classes.modal}>
+                        <Typography variant="body2" className={classNames(classes['card-font'], classes.title)}>{beer.name}</Typography>                    
+                    </div>
+                </Modal>
+            </React.Fragment>
+
         )
     }
 }
